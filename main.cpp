4b66781ec0bca244d8f8024024b91b841b59f3ea
Y99dr451l <matthias@kockisch.eu>
2022-07-29 23:28:22 +0200

    refactored, added air ressitance and fps counter

--- main.cpp
+++ main.cpp
@@ -56,61 +56,132 @@ struct PhysicalObject : public sf::Drawable {
     }
 };
 
-void applyConstraint(std::vector<PhysicalObject>& objects) {
-    const sf::Vector2f bposition(500, 500); const float bradius = 400;
-    for (int i = 0; i < objects.size(); i++) {
-        if (objects[i].is_static) continue;
-        sf::Vector2f distance = objects[i].position - bposition;
-        float distance_length = sqrt(distance.x * distance.x + distance.y * distance.y);
-        if (distance_length > bradius - objects[i].radius) {
-            sf::Vector2f normal = distance / distance_length;
-            objects[i].position = bposition + normal * (bradius - objects[i].radius);
+struct Spring {
+    Spring(PhysicalObject* a, PhysicalObject* b, float spring_constant, float damping_constant) {
+        this->a = a; this->b = b;
+        this->spring_constant = spring_constant;
+        this->damping_constant = damping_constant;
+    }
+    PhysicalObject* a; PhysicalObject* b;
+    float spring_constant, damping_constant;
+};
+
+// struct Force {
+//     Force(PhysicalObject* a, float x, float y) {
+//         this->a = a;
+//         this->x = x; this->y = y;
+//     }
+//     PhysicalObject* a;
+//     float x, y;
+// };
+
+struct Scene {
+    Scene(sf::Vector2f gravity = sf::Vector2f(0, 0), float air_resistance = 0.f) {
+        this->gravity = gravity;
+        this->air_resistance = air_resistance;
+    }
+    std::vector<PhysicalObject*> objects;
+    std::vector<Spring*> springs;
+    // std::vector<Force*> forces;
+    sf::Vector2f gravity;
+    float air_resistance;
+    bool elastic_collisions = true;
+
+    void applyForces() {
+        for (int i = 0; i < objects.size(); i++) {
+            PhysicalObject* a = objects[i];
+            a->acceleration += gravity;
+            if (air_resistance != 0.f) {
+                a->acceleration.x += air_resistance * a->velocity.x;
+                a->acceleration.y += air_resistance * a->velocity.y;
+            }
         }
     }
-}
 
-void solveCollisions(std::vector<PhysicalObject>& objects) {
-    for (int i = 0; i < objects.size(); i++) for (int j = i+1; j < objects.size(); j++) {
-            if (objects[i].is_static && objects[j].is_static) continue;
-            float distance = sqrt(pow(objects[i].position.x - objects[j].position.x, 2) + pow(objects[i].position.y - objects[j].position.y, 2));
-            if (distance < objects[i].radius + objects[j].radius) {
-                const sf::Vector2f normal = (objects[i].position - objects[j].position) / distance;
-                const sf::Vector2f delta = normal * (objects[i].radius + objects[j].radius - distance) * 0.5f;
-                objects[i].position += delta; objects[j].position -= delta;
+    void applyConstraint() {
+        const sf::Vector2f bposition(500, 500); const float bradius = 400;
+        for (int i = 0; i < objects.size(); i++) {
+            if (objects[i]->is_static) continue;
+            sf::Vector2f distance = objects[i]->position - bposition;
+            float distance_length = sqrt(distance.x * distance.x + distance.y * distance.y);
+            if (distance_length > bradius - objects[i]->radius) {
+                sf::Vector2f normal = distance / distance_length;
+                objects[i]->position = bposition + normal * (bradius - objects[i]->radius);
             }
+        }
     }
-}
 
-void solveElasticCollisions(std::vector<PhysicalObject>& objects) {
-    for (int i = 0; i < objects.size(); i++) for (int j = i+1; j < objects.size(); j++) {
-            if (objects[i].is_static && objects[j].is_static) continue;
-            float sqdist = pow(objects[i].position.x - objects[j].position.x, 2) + pow(objects[i].position.y - objects[j].position.y, 2);
-            if (sqdist < pow(objects[i].radius + objects[j].radius, 2)) {
-                const float mass_sum = objects[i].mass + objects[j].mass;
-                const sf::Vector2f posdiff = objects[i].position - objects[j].position;
-				const sf::Vector2f veldiff = objects[i].velocity - objects[j].velocity;
-				const float dot = posdiff.x*veldiff.x + posdiff.y*veldiff.y;
-				objects[i].velocity -= 2*objects[j].mass*dot*posdiff/(mass_sum*sqdist);
-				objects[j].velocity += 2*objects[i].mass*dot*posdiff/(mass_sum*sqdist);
-
-				const float distance = sqrt(sqdist);
-                const sf::Vector2f delta = posdiff/distance*(objects[i].radius + objects[j].radius - distance)*0.5f;
-                objects[i].position += delta; objects[j].position -= delta;
-            }
+    void solveCollisions() {
+        for (int i = 0; i < objects.size(); i++) for (int j = i+1; j < objects.size(); j++) {
+                if (objects[i]->is_static && objects[j]->is_static) continue;
+                float distance = sqrt(pow(objects[i]->position.x - objects[j]->position.x, 2) + pow(objects[i]->position.y - objects[j]->position.y, 2));
+                if (distance < objects[i]->radius + objects[j]->radius) {
+                    const sf::Vector2f normal = (objects[i]->position - objects[j]->position) / distance;
+                    const sf::Vector2f delta = normal * (objects[i]->radius + objects[j]->radius - distance) * 0.5f;
+                    objects[i]->position += delta; objects[j]->position -= delta;
+                }
+        }
     }
-}
+
+    void solveElasticCollisions() {
+        for (int i = 0; i < objects.size(); i++) for (int j = i+1; j < objects.size(); j++) {
+                if (objects[i]->is_static && objects[j]->is_static) continue;
+                float sqdist = pow(objects[i]->position.x - objects[j]->position.x, 2) + pow(objects[i]->position.y - objects[j]->position.y, 2);
+                if (sqdist < pow(objects[i]->radius + objects[j]->radius, 2)) {
+                    const float mass_sum = objects[i]->mass + objects[j]->mass;
+                    const sf::Vector2f posdiff = objects[i]->position - objects[j]->position;
+                    const sf::Vector2f veldiff = objects[i]->velocity - objects[j]->velocity;
+                    const float dot = posdiff.x*veldiff.x + posdiff.y*veldiff.y;
+                    objects[i]->velocity -= 2*objects[j]->mass*dot*posdiff/(mass_sum*sqdist);
+                    objects[j]->velocity += 2*objects[i]->mass*dot*posdiff/(mass_sum*sqdist);
+
+                    const float distance = sqrt(sqdist);
+                    const sf::Vector2f delta = posdiff/distance*(objects[i]->radius + objects[j]->radius - distance)*0.5f;
+                    objects[i]->position += delta; objects[j]->position -= delta;
+                }
+        }
+    }
+
+    void update(float dt) {
+        for (int i = 0; i < objects.size(); i++) objects[i]->updateObject(dt);
+        for (int i = 0; i < springs.size(); i++) {
+            sf::Vector2f distance = springs[i]->b->position - springs[i]->a->position;
+            float distance_length = sqrt(distance.x * distance.x + distance.y * distance.y);
+            float force = springs[i]->spring_constant * (distance_length - springs[i]->a->radius - springs[i]->b->radius);
+            sf::Vector2f force_vector = distance / distance_length * force;
+            springs[i]->a->applyAcceleration(force_vector);
+            springs[i]->b->applyAcceleration(-force_vector);
+        }
+        // for (int i = 0; i < forces.size(); i++) {
+        //     forces[i].a->applyAcceleration(forces[i].x, forces[i].y);
+        // }
+        for (int i = 0; i < objects.size(); i++)
+            objects[i]->applyAcceleration(gravity + sf::Vector2f(-air_resistance*objects[i]->velocity.x, -air_resistance*objects[i]->velocity.y));
+        if (elastic_collisions) solveElasticCollisions(); else solveCollisions();
+        applyConstraint();
+    }
+
+    void draw(sf::RenderWindow& window) {
+        for (int i = 0; i < objects.size(); i++) window.draw(*objects[i]);
+    }
+};
 
 int main() {
     sf::Clock clock;
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "pure chaos");
-    window.setFramerateLimit(60); // window.setMouseCursorVisible(false);
-
-    std::vector<PhysicalObject> objects;
+    // window.setFramerateLimit(60); // window.setMouseCursorVisible(false);
+    
+    sf::Font font; sf::Text text;
+    font.loadFromFile("Lavinia.otf");
+    text.setFont(font); text.setCharacterSize(20); text.setPosition(5, 5);
+    
+    Scene scene(sf::Vector2f(0, 50), 1.f);
+    scene.elastic_collisions = true;
     for (int i = 0; i < 15; i++) for (int j = 0; j < 15; j++) {
-		objects.push_back(PhysicalObject(250 + i*20, 250 + j*20, 9, 250));
-		objects.back().velocity = sf::Vector2f(rand()%100 - 50, rand()%100 - 50);
+		scene.objects.push_back(new PhysicalObject(250 + i*20, 250 + j*20, 9, 250));
+		scene.objects.back()->velocity = sf::Vector2f(rand()%1000 - 500, rand()%1000 - 500);
 	}
-	objects.push_back(PhysicalObject(500, 500, 100, 1e10));
+	scene.objects.push_back(new PhysicalObject(500, 500, 100, 1e10, true));
 
     bool spacepressed = false;
     while (window.isOpen()) {
@@ -118,21 +189,17 @@ int main() {
         while (window.pollEvent(event))
             if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) window.close();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !spacepressed) {
-            objects.push_back(PhysicalObject(rand() % 1000, rand() % 1000, rand() % 100 + 10));
+            scene.objects.push_back(new PhysicalObject(rand() % 1000, rand() % 1000, rand() % 100 + 10));
             spacepressed = true;
         } else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) spacepressed = false;
 
         float dt = clock.restart().asSeconds();
-        for (int i = 0; i < SUB_STEPS; i++) {
-			 for (int j = 0; j < objects.size(); j++) {
-                // objects[j].applyAcceleration(0, 50);
-                objects[j].updateObject(dt/SUB_STEPS);
-            }
-            // solveCollisions(objects);
-            solveElasticCollisions(objects);
-        }
+        scene.update(dt);
+        text.setString("FPS: " + std::to_string(1/dt));
+
         window.clear();
-        for (int i = 0; i < objects.size(); i++) window.draw(objects[i]);
+        scene.draw(window);
+        window.draw(text);
         window.display();
     }
     return 0;
